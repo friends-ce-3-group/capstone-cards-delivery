9dc2e0ca02112fc6e33f2288974f67a2d6407610
Low Chee Meng <lowcheemeng02@gmail.com>
2023-11-15 12:20:18 +0800

    simplify lambda

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,17 +1,10 @@
 import json
 import boto3
 
-def process_raw_query(qstr):
-    qstr = [i for i in qstr.split("&")]
-    qdict = {}
-    for q in qstr:
-        vars = q.split("=")
-        qdict[vars[0]]=vars[1]
-    return qdict
 
 def ${lambda_handler_function}(event, context):
 
-    inputs = process_raw_query(event["rawQueryString"])
+    inputs = json.loads(event['body'])
     inputs["Lambda function ARN"] = context.invoked_function_arn
     inputs["CloudWatch log group name"] = context.log_group_name
 
