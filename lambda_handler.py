eea8894b0878dd061b2c91fb5f3f08b2791e2823
Low Chee Meng <lowcheemeng02@gmail.com>
2023-11-16 12:50:59 +0800

    test lambda function

--- lambda_handler.py
+++ lambda_handler.py
@@ -4,15 +4,14 @@ import boto3
 
 def ${lambda_handler_function}(event, context):
 
-    inputs = json.loads(event['body'])
-    inputs["Lambda function ARN"] = context.invoked_function_arn
-    inputs["CloudWatch log group name"] = context.log_group_name
-
     client = boto3.client('sns')
     
+    out = {} 
+    out['default'] = json.dumps(event)
+
     response = client.publish (
         TopicArn = "${sns_topic_arn}",
-        Message = json.dumps({'default': inputs}),
+        Message = json.dumps(out),
         MessageStructure = 'json'
     )
     
