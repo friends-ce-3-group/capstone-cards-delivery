14e3de581afd427e2189ef4fa0998c2ee2c22bd8
Low Chee Meng <lowcheemeng02@gmail.com>
2023-11-21 20:13:12 +0800

    modify sns

--- sns.tf
+++ sns.tf
@@ -1,5 +1,5 @@
 locals {
-  sns_tag = "${var.resource_grp_name}-sns"
+  sns_tag = "${var.resource_grp_name}-ses-status"
 }
 
 resource "aws_sns_topic" "notification" {
@@ -15,4 +15,18 @@ resource "aws_sns_topic_subscription" "notification" {
   topic_arn = aws_sns_topic.notification.arn
   protocol  = "email"
   endpoint  = "gooodgreets@gmail.com"
+}
+
+resource "aws_ses_configuration_set" "example_config_set" {
+  name = "${var.resource_grp_name}-ses-status"
+}
+
+resource "aws_ses_event_destination" "example_event_destination" {
+  name               = "${var.resource_grp_name}-ses-destination"
+  matching_types    = ["send", "reject", "bounce", "complaint", "delivery"]
+  configuration_set_name = aws_ses_configuration_set.example_config_set.name
+  
+  sns_destination {
+    topic_arn = aws_sns_topic.notification.arn
+  }
 }
